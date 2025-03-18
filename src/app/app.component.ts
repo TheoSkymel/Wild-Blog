abf131d35935238a7bdb2abd5c7b41ea4d5e8e94
SkymelTheo <erigoni.t@gmail.com>
2025-03-18 16:52:27 +0100

    affichage header ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './HeaderComponents/header.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Bienvenue sur le Wild Blog de Theo !';
 }
+
